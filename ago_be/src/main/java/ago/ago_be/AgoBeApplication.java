3205d2cbd50d3405e0e7284b1fba7f8bfcdad6a8
주한음 <jhnote@juhan-eum-ui-MacBookPro.local>
2022-08-01 20:40:47 +0900

    Initial commit

---
+++ ago_be/src/main/java/ago/ago_be/AgoBeApplication.java
@@ -0,0 +1,13 @@
+package ago.ago_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgoBeApplication.class, args);
+	}
+
+}
---
+++ ago_be/src/test/java/ago/ago_be/AgoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ago.ago_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
